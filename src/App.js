35bccf5368bdd8e64ec09fd133516a1bc86af3b1
Therese Lybo <therese.lybo@gmail.com>
2021-02-27 19:37:52 +0100

    episode 4

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import DadJokes from "./DadJokes";
 function App() {
   return (
     <>
-      <h1>Info screen displaying dadjokes</h1>
       <DadJokes />
     </>
   );
--- src/DadJokes.js
+++ src/DadJokes.js
@@ -1,10 +1,22 @@
+import { useState, useEffect } from "react";
+
 export default function DadJokes() {
-  function getJokes() {
-    console.log("getting jokes");
+  const [joke, setJoke] = useState({});
+
+  useEffect(() => {
     fetch("https://www.reddit.com/r/dadjokes/top.json?t=day&limit=1")
       .then((response) => response.json())
-      .then((result) => console.log(result.data.children[0].data));
-  }
-  getJokes();
-  return <h1>Dad Jokes</h1>;
+      .then((result) => setJoke(result.data.children[0].data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  console.log(joke);
+
+  return (
+    <div className="joke">
+      <h1>Dad Joke of the day</h1>
+      <h2>{joke.title}</h2>
+      <p>{joke.selftext}</p>
+    </div>
+  );
 }
